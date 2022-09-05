0105e685104ffd6dfe5aa31293ec5546d671354e
OneKit <onekit@onekit.cn>
2022-09-05 16:53:33 +0800

    Update app.js

--- demo/app.js
+++ demo/app.js
@@ -1,8 +1,8 @@
 
 App({
     onLaunch(){
-        //this.onekit_path = "https://onekit.cn/examples/"
-        this.onekit_path = "http://localhost/examples/"
+        this.onekit_path = "https://onekit.cn/examples/"
+        //this.onekit_path = "http://localhost/examples/"
     }
 });
 
