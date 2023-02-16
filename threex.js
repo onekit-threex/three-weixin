e32bbf091f7a02e7b79959b6db83904afec978d7
Zhang <zhangjin_va@qq.com>
2023-02-16 22:37:49 +0800

    Update threex.js

--- threex.js
+++ threex.js
@@ -2,10 +2,10 @@ module.exports = {
     getApp() {
         return getApp()
     },
-    getCurrentPages() {
-        return getCurrentPages()
-    },
     wx_request() {
         return wx.request
+    }
+    getCurrentPages() {
+        return getCurrentPages()
     }
 }
